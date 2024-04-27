2e88b53947ec515856af314a54ef09773b63d484
inspizzz <wiktor.wiejak@gmail.com>
2024-04-27 23:28:04 +0100

    added functionality for checking if the user is already logged in and also for loggin in

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,14 +3,16 @@
 import Link from "next/link";
 import { TopBar } from "@/components/User/TopBar";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // icons
 import { MdAlternateEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { useUser } from "@/contexts/userContext";
 
 export default function Login() {
 
+	const [ mounted, setMounted ] = useState(false)
 
 	const [ focus1, setFocus1 ] = useState(false)
 	const [ focus2, setFocus2 ] = useState(false)
@@ -18,75 +20,104 @@ export default function Login() {
 	const [ username, setUsername ] = useState("")
 	const [ password, setPassword ] = useState("")
 
+	const { login, user } = useUser()
+
 	const Submit = (e) => {
 		e.preventDefault()
 
-		console.log("Submitted")
-
 		console.log(username)
 		console.log(password)
+
+		login(username, password)
 		
 	}
-	return (
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	return mounted && (
 		<div className="w-full h-full bg-primary flex flex-col justify-center">
 			<TopBar name="Up" />
 
-			<form className="w-1/3 h-1/2 bg-accent self-center flex justify-center gap-10 z-20 rounded-2xl p-8 min-w-96">
-				<div className="w-1/2 h-full flex flex-col gap-2 justify-between">
+			{
+				(user === null) ? (
+					<form className="w-1/3 h-1/2 bg-accent self-center flex justify-center gap-10 z-20 rounded-2xl p-8 min-w-96" onSubmit={Submit}>
+						<div className="w-1/2 h-full flex flex-col gap-2 justify-between">
 
-					<div className="flex flex-col gap-8">
-						<h1 className="text-white font-extrabold text-4xl">Sign In</h1>
+							<div className="flex flex-col gap-8">
+								<h1 className="text-white font-extrabold text-4xl">Sign In</h1>
 
-						<div className="flex flex-col gap-2">
-							{/* email */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									<MdAlternateEmail className="text-white self-center" />
-									<input
-										type="text"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Email"
-										onFocus={() => setFocus1(true)}
-										onBlur={() => setFocus1(false)}
-									/>
+								<div className="flex flex-col gap-2">
+									{/* email */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											<MdAlternateEmail className="text-white self-center" />
+											<input
+												type="text"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Email"
+												onFocus={() => setFocus1(true)}
+												onBlur={() => setFocus1(false)}
+												onChange={(e) => setUsername(e.target.value)}
+											/>
+										</div>
+										<div className={focus1 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+
+									{/* password */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											<RiLockPasswordLine className="text-white self-center" />
+											<input
+												type="password"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Password"
+												onFocus={() => setFocus2(true)}
+												onBlur={() => setFocus2(false)}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</div>
+										<div className={focus2 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+									<Link href={"register"} className="underline text-white w-fit">Dont have an account?</Link>
 								</div>
-								<div className={focus1 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
 							</div>
 
-							{/* password */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									<RiLockPasswordLine className="text-white self-center" />
-									<input
-										type="password"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Password"
-										onFocus={() => setFocus2(true)}
-										onBlur={() => setFocus2(false)}
-									/>
-								</div>
-								<div className={focus2 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+							<div>
+								<button type="submit" className="w-1/2 bg-secondary rounded-sm p-2 mt-4 cursor-pointer hover:shadow-2xl hover:shadow-white">Login</button>
 							</div>
 						</div>
+
+						<div className="w-1/2 h-full flex flex-col justify-center">
+							<Image 
+								src="/login_image.webp"
+								className="w-full h- self-center"
+								alt={"not found"} 
+								width={"200"}
+								height={"200"}
+								priority
+							/>
+						</div>
+					</form>
+				) : (
+					<div className="w-1/3 h-1/2 bg-accent self-center flex flex-col justify-center gap-10 z-20 rounded-2xl p-8 min-w-96">
+						<h1 className="text-white text-2xl font-bold self-center">Oops! Your already logged in</h1>
+						<div className="w-3/4 h-fit flex flex-col justify-center self-center">
+							<Image 
+								src="/login_image.webp"
+								className="w-full h- self-center"
+								alt={"not found"} 
+								width={"300"}
+								height={"200"}
+								priority
+							/>
+						</div>
 					</div>
+				)
+			}
 
-
-					<div>
-						<button type="submit" className="w-1/2 bg-secondary rounded-sm p-2 mt-4 cursor-pointer hover:shadow-2xl hover:shadow-white">Login</button>
-					</div>
-					
-				</div>
-
-				<div className="w-1/2 h-full flex flex-col justify-center">
-					<Image 
-						src="/register_image.webp"
-						className="w-full h- self-center"
-						alt={"not found"} 
-						width={"200"}
-						height={"200"}/>
-
-				</div>
-			</form>
+			
 		</div>
 
 	)
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -3,16 +3,19 @@
 import Link from "next/link";
 import { TopBar } from "@/components/User/TopBar";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // icons
 import { CiUser } from "react-icons/ci";
 import { MdAlternateEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { useUser } from "@/contexts/userContext";
 
 
 
 export default function RegisterPage() {
+
+	const [ mounted, setMounted ] = useState(false)
 
 	const [ name, setName ] = useState("")
 	const [ email, setEmail ] = useState("")
@@ -24,6 +27,11 @@ export default function RegisterPage() {
 	const [ focus3, setFocus3 ] = useState(false)
 	const [ focus4, setFocus4 ] = useState(false)
 
+	const { user } = useUser()
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
 	/**
 	 * perform checks on the input fields
 	 */
@@ -42,103 +50,123 @@ export default function RegisterPage() {
 		console.log("can register")
 	}
 
-	return (
+	return mounted && (
 		<div className="w-full h-full bg-primary flex flex-col justify-center">
 			<TopBar name="Up" />
 
-			<form className="w-1/3 h-1/2 bg-accent self-center flex justify-center gap-10 z-20 rounded-2xl p-8 min-w-96">
-				<div className="w-1/2 h-full flex flex-col gap-2 justify-between">
+			{
+				(user === null) ? (
+					<form className="w-1/3 h-1/2 bg-accent self-center flex justify-center gap-10 z-20 rounded-2xl p-8 min-w-96">
+						<div className="w-1/2 h-full flex flex-col gap-2 justify-between">
 
-					<div className="flex flex-col gap-8">
-						<h1 className="text-white font-extrabold text-4xl">Sign Up</h1>
+							<div className="flex flex-col gap-8">
+								<h1 className="text-white font-extrabold text-4xl">Sign Up</h1>
 
-						<div className="flex flex-col gap-2">
-							{/* username */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									<CiUser className="text-white self-center" />
-									<input
-										type="text"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Username"
-										onFocus={() => setFocus1(true)}
-										onBlur={() => setFocus1(false)}
-									/>
+								<div className="flex flex-col gap-2">
+									{/* username */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											<CiUser className="text-white self-center" />
+											<input
+												type="text"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Username"
+												onFocus={() => setFocus1(true)}
+												onBlur={() => setFocus1(false)}
+											/>
+										</div>
+										<div className={focus1 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+									
+									{/* email */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											<MdAlternateEmail className="text-white self-center" />
+											<input
+												type="text"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Email"
+												onFocus={() => setFocus2(true)}
+												onBlur={() => setFocus2(false)}
+											/>
+										</div>
+										<div className={focus2 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+
+									{/* password */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											<RiLockPasswordLine className="text-white self-center" />
+											<input
+												type="password"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Password"
+												onFocus={() => setFocus3(true)}
+												onBlur={() => setFocus3(false)}
+											/>
+										</div>
+										<div className={focus3 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+
+									{/* password confirm */}
+									<div className="flex flex-col">
+										<div className="flex gap-2 w-full h-fit">
+											{/* <CiUser className="text-white self-center" /> */}
+											<input 
+												type="password"
+												className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
+												placeholder="Repeat your password"
+												onFocus={() => setFocus4(true)}
+												onBlur={() => setFocus4(false)}
+												autoComplete="off"
+												onPaste={(e) => {e.preventDefault()}}
+											/>
+										</div>
+										<div className={focus4 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+									</div>
+
+									<Link href={"login"} className="underline text-white w-fit">Already have an account?</Link>
 								</div>
-								<div className={focus1 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
+
+								<div className="flex gap-2">
+									<input type="checkbox" className="self-center" />
+									<p className="text-white">I agree to <Link href={"/terms"} className="underline cursor-pointer">Terms & Conditions</Link></p>
+								</div>
+							</div>
+
+
+							<div>
+								<button type="submit" className="w-1/2 bg-secondary rounded-sm p-2 cursor-pointer hover:shadow-2xl hover:shadow-white">Register</button>
 							</div>
 							
-							{/* email */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									<MdAlternateEmail className="text-white self-center" />
-									<input
-										type="text"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Email"
-										onFocus={() => setFocus2(true)}
-										onBlur={() => setFocus2(false)}
-									/>
-								</div>
-								<div className={focus2 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
-							</div>
-
-							{/* password */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									<RiLockPasswordLine className="text-white self-center" />
-									<input
-										type="password"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Password"
-										onFocus={() => setFocus3(true)}
-										onBlur={() => setFocus3(false)}
-									/>
-								</div>
-								<div className={focus3 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
-							</div>
-
-							{/* password confirm */}
-							<div className="flex flex-col">
-								<div className="flex gap-2 w-full h-fit">
-									{/* <CiUser className="text-white self-center" /> */}
-									<input 
-										type="password"
-										className="w-full border-none bg-accent p-2 rounded-sm outline-none text-white"
-										placeholder="Repeat your password"
-										onFocus={() => setFocus4(true)}
-										onBlur={() => setFocus4(false)}
-										autoComplete="off"
-										onPaste={(e) => {e.preventDefault()}}
-									/>
-								</div>
-								<div className={focus4 ? "bg-secondary w-full h-[1px]" : "bg-white w-full h-[1px]"}/>
-							</div>
 						</div>
 
-						<div className="flex gap-2">
-							<input type="checkbox" className="self-center" />
-							<p className="text-white">I agree to <Link href={"/terms"} className="underline cursor-pointer">Terms & Conditions</Link></p>
+						<div className="w-1/2 h-full flex flex-col justify-center">
+							<Image 
+								src="/register_image.webp"
+								className="w-full h- self-center"
+								alt={"not found"} 
+								width={"200"}
+								height={"200"}/>
+
+						</div>
+					</form>
+				) : (
+					<div className="w-1/3 h-1/2 bg-accent self-center flex flex-col justify-center gap-10 z-20 rounded-2xl p-8 min-w-96">
+						<h1 className="text-white text-2xl font-bold self-center">Oops! Your already logged in</h1>
+						<div className="w-3/4 h-fit flex flex-col justify-center self-center">
+							<Image 
+								src="/register_image.webp"
+								className="w-full h- self-center"
+								alt={"not found"} 
+								width={"300"}
+								height={"200"}
+								priority
+							/>
 						</div>
 					</div>
-
-
-					<div>
-						<button type="submit" className="w-1/2 bg-secondary rounded-sm p-2 mt-4 cursor-pointer hover:shadow-2xl hover:shadow-white">Register</button>
-					</div>
-					
-				</div>
-
-				<div className="w-1/2 h-full flex flex-col justify-center">
-					<Image 
-						src="/login_image.webp"
-						className="w-full h- self-center"
-						alt={"not found"} 
-						width={"200"}
-						height={"200"}/>
-
-				</div>
-			</form>
+				)
+			}
 		</div>
 	)
 }
