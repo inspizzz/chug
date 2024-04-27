29f8303fde64cb923993324aa21aa2876c7a7672
inspizzz <wiktor.wiejak@gmail.com>
2024-04-27 23:28:20 +0100

    created the feed portion of the app

---
+++ app/feed/layout.jsx
@@ -0,0 +1,7 @@
+export default function FeedLayout({ children }) {
+	return (
+		<div className="h-fit w-full px-96 py-10 bg-primary">
+			{children}
+		</div>
+	)
+}
---
+++ app/feed/page.jsx
@@ -0,0 +1,27 @@
+import { Posts } from "@/components/Feed/Posts";
+import { UserSuggestions } from "@/components/Feed/UserSuggestions";
+import { TopBar } from "@/components/User/TopBar";
+
+// icons
+import { IoIosAddCircleOutline } from "react-icons/io";
+
+
+export default function FeedPage() {
+	return (
+		<div className="h-full w-full bg-primary flex flex-col gap-4">
+			<TopBar name="Dash" />
+
+			<div className="w-full h-16 bg-accent flex justify-between p-2 rounded-2xl z-20">
+				<p className="text-white text-2xl font-extrabold self-center">Your Feed</p>
+
+				<div className="h-full w-fit hover:shadow-md rounded-full hover:shadow-white z-20">
+					<IoIosAddCircleOutline className="h-full w-fit aspect-square" color="white"/>
+				</div>
+			</div>
+
+			<UserSuggestions />
+
+			<Posts />
+		</div>
+	)
+}	
