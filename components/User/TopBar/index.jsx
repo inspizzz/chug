f2517d9707573f0fed566ebb1a4a36c78084dacf
inspizzz <wiktor.wiejak@gmail.com>
2024-04-27 23:30:31 +0100

    updated the top bar to be shown if logged in and to show log in and register buttons if the user has is not logged in

--- components/User/TopBar/index.jsx
+++ components/User/TopBar/index.jsx
@@ -1,18 +1,49 @@
-import Link from 'next/link'
+'use client'
 
-export function TopBar({ name="" }) {
-	return (
-		<div className="absolute top-0 w-full h-20 flex justify-between pl-4 z-20">
+import { useUser } from '@/contexts/userContext'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+import { MdLogout } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { MdHome } from "react-icons/md";
+import { CgNotes } from "react-icons/cg";
+import { RxCross1 } from "react-icons/rx";
+
+export function TopBar({ name = "" }) {
+	const [selected, setSelected] = useState(false)
+	const [mounted, setMounted] = useState(false)
+
+	const { user, logout } = useUser()
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	return mounted && (
+		<div className="absolute top-0 left-0 w-full h-20 flex justify-between pl-4 z-20">
 			<div className="self-center w-fit h-full flex justify-center">
 				<Link href={"/"} className="text-secondary self-center font-extrabold text-4xl cursor-pointer select-none">Chug<span className='text-white font-extrabold text-4xl cursor-pointer select-none'>{name}</span></Link>
 			</div>
 
-			<div className="self-center w-48 h-full flex justify-center p-4">
+			<div className="self-center w-fit h-full flex justify-center p-4">
 				{
 					// is the user logged in?
-					false ? (
-						<div className="h-full aspect-square rounded-full bg-secondary self-center">
+					(user !== null) ? (
+						<div className='w-fit h-16 flex'>
 
+							{
+								selected && (
+									<div className="bg-notes_background w-full rounded-2xl flex gap-1 p-1">
+										<Link href={"/feed"} className="aspect-square rounded-2xl bg-white hover:bg-secondary flex justify-center"><MdHome className="self-center text-black" /></Link>
+										<Link href={"/profile"} className="aspect-square rounded-2xl bg-white hover:bg-secondary flex justify-center"><CgProfile className="self-center text-black" /></Link>
+										<Link href={"/settings"} className="aspect-square rounded-2xl bg-white hover:bg-secondary flex justify-center"><IoSettingsOutline className="self-center text-black" /></Link>
+									</div>
+								)
+							}
+
+							<div className="h-16 w-16 aspect-square rounded-full bg-secondary self-center" onClick={() => setSelected(!selected)} />
 						</div>
 					) : (
 						<div className="h-full w-full self-center flex justify-center gap-2 text-balance">
@@ -21,9 +52,9 @@ export function TopBar({ name="" }) {
 						</div>
 					)
 				}
-				
+
 			</div>
 
 		</div>
-  	)
+	)
 }
